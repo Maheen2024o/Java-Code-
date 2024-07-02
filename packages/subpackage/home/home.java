fc830f5ac11500c50fc89812c2fd5557fdfd7304
Maheen2024o <maheen.mansha@emumba.com>
2024-07-02 09:51:57 +0500

    Add files via upload

---
+++ packages/subpackage/home/home.java
@@ -0,0 +1,10 @@
+package packages.subpackage.home;
+
+public class home {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+	}
+
+}
---
+++ packages/subpackage/newpack/packageSubPackage.java
@@ -0,0 +1,11 @@
+package packages.subpackage.newpack;
+import Test;
+public class packageSubPackage {
+
+	public static void main(String[] args) {
+		
+		System.out.println("I am in the subpackage");
+
+	}
+
+}
